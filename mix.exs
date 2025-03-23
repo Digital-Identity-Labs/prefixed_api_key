76a6e6c23ad9a98d972217cc151eecd4b9403c20
Pete Birkinshaw <pete@binary-ape.org>
2025-03-23 14:04:45 +0000

    Update deps slightly

--- mix.exs
+++ mix.exs
@@ -40,15 +40,15 @@ defmodule PrefixedApiKey.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:puid, "~> 2.2"},
+      {:puid, "~> 2.3"},
       {:apex, "~> 1.2", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.14 and >= 0.14.4", only: [:dev, :test]},
-      {:benchee, "~> 1.0.1", only: [:dev, :test]},
-      {:ex_doc, "~> 0.23.0", only: :dev, runtime: false},
-      {:earmark, "~> 1.3", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
-      {:doctor, "~> 0.17.0", only: :dev, runtime: false}
+      {:excoveralls, "~> 0.18", only: [:dev, :test]},
+      {:benchee, "~> 1.3", only: [:dev, :test]},
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
+      {:earmark, "~> 1.4", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
+      {:doctor, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 
